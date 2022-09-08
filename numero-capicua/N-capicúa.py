b7c0126815973b1ca155ce97cb99c9c9411df9c7
Juan guillermo Peñaloza ibañez <102609169+JuanGPI@users.noreply.github.com>
2022-09-08 10:15:01 -0500

    Add files via upload

---
+++ numero-capicua/N-capicúa.py
@@ -0,0 +1,10 @@
+print("---------------------------")
+print("------Numero-Capicúa-------")
+print("---------------------------")
+
+#input
+x = input("ingrese el valor")
+if x == x[::-1]:
+    print("el numero es capicúa: ")
+else:
+    print("el numero no es capicúa: ")
